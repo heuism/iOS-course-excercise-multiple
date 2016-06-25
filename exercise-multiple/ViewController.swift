154e8e42449f355701192e7fcbea6c7b8ac45280
Hien Tran <heuism23892@gmail.com>
2016-06-25 15:03:06 +1000

    Currently finish

--- exercise-multiple/ViewController.swift
+++ exercise-multiple/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var inputNum: Int = 0
+    var result: Int = 0
+    let MAX_NUM = 50
+    var displayStr: String = "Press Add to add!"
+    
+    @IBOutlet weak var appName: UIImageView!
+    @IBOutlet weak var inputNumber: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var resultDisplay: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBAction func playGame(sender: AnyObject) {
+        if inputNumber.text != nil && inputNumber.text != "" {
+            appName.hidden = true
+            inputNumber.hidden = true
+            playBtn.hidden = true
+            
+            resultDisplay.hidden = false
+            addBtn.hidden = false
+            
+            //resultDisplay.text = "Press Add to add!"
+            updateResult()
+            result = 0
+            inputNum = Int(inputNumber.text!)!
+            inputNumber.text = ""
+            print(inputNum)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addNumber(sender: AnyObject) {
+        print("Here")
+        if(result + inputNum <= MAX_NUM){
+            displayStr = "\(result) + \(inputNum) = \(result+inputNum)"
+            result += inputNum
+            print(result)
+            //resultDisplay.text = displayStr
+            updateResult()
+        }
+        else{
+            resetGame()
+        }
     }
-
-
+    
+    func updateResult() {
+        resultDisplay.text = displayStr
+    }
+    
+    func resetGame() {
+        appName.hidden = false
+        inputNumber.hidden = false
+        playBtn.hidden = false
+        
+        resultDisplay.hidden = true
+        addBtn.hidden = true
+    }
 }
 
